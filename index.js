c4b9c416d379c1e8e9ff0b63695cff6af2c2d22a
MAC <mac@MACs-MacBook-Pro.local>
2022-12-01 23:03:46 +0500

    inital commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const port = process.env.port || 8000;
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/ABC', (req, res) => {
     res.send('Hello World!');
 });
 
